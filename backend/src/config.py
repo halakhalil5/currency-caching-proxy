3cee7bfbb51ba26c5e825f97f69fd2e4ef4942c2
halakhalil5 <halahossam123.hh@gmail.com>
2026-06-18 15:26:54 +0300

    .

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,8 +3,8 @@ import os
 UPSTREAM_URL = os.getenv("UPSTREAM", "https://open.er-api.com/v6/latest")
 TIMEOUT = int(os.getenv("timeout", "6"))
 TTL=int(os.getenv("TTL", "60"))
-BACKOFF=1
-MAX_RETRIES=5
+BACKOFF=int(os.getenv("Backoff", "1"))
+MAX_RETRIES=int(os.getenv("MaxRetries", "5"))
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
